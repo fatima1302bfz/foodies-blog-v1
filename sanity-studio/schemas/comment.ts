ab1867a4826e0c5dfcb1a41c77d2e6ef5cdf49c1
fatima1302bfz <boukhatem.fatimaa4@gmail.com>
2023-03-10 11:22:16 +0100

    new commit

---
+++ sanity-studio/schemas/comment.ts
@@ -0,0 +1,33 @@
+export default {
+  name: 'comment',
+  type: 'document',
+  title: 'Comment',
+  fields: [
+    {
+      title: 'Username',
+      name: 'username',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Avatar',
+      name: 'avatar',
+      type: 'string',
+    },
+    {
+      title: 'Comment',
+      name: 'comment',
+      type: 'text',
+    },
+    {
+      name: 'post',
+      type: 'reference',
+      to: [{
+        type: 'post'}],
+    },
+  ],
+}
---
+++ sanity-studio/schemas/index.ts
@@ -0,0 +1,7 @@
+import post from './post'
+import author from './author'
+import comment from './comment'
+import category from './category'
+import blockContent from './blockContent'
+
+export const schemaTypes = [post, author, category, blockContent, comment]
